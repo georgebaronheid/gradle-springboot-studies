7667af0d4e435cbb52909e57c3fe7a2bc5db9145
George Baronheid <baronheid.george@gmail.com>
2020-04-28 20:02:23 -0300

    Provided config to h2 at yaml file and created product entity

---
+++ src/main/java/br/com/baronheid/gradlespringbootstudies/model/entity/Product.java
@@ -0,0 +1,35 @@
+package br.com.baronheid.gradlespringbootstudies.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product")
+    private int id;
+
+    @NotBlank(message = "Name required")
+    @Size(max = 50)
+    private String name;
+
+    @Min(value = 0, message = "Price can not be negative")
+    private double price;
+
+    private boolean newProdut;
+
+    @Past
+    private LocalDate manufacteDate;
+
+}
