665d7b4daf7dfbe7978a964d1c10d8b1a6e5499d
George Baronheid <baronheid.george@gmail.com>
2020-04-28 17:26:30 -0300

    First commit

---
+++ src/main/java/br/com/baronheid/gradlespringbootstudies/GradleSpringbootStudiesApplication.java
@@ -0,0 +1,13 @@
+package br.com.baronheid.gradlespringbootstudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringbootStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleSpringbootStudiesApplication.class, args);
+	}
+
+}
